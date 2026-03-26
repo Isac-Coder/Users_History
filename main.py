73edc6573ce01d2dccd141bc72be24bb68b5fa45
Isac <isaacalvarez0516@gmail.com>
2026-03-26 15:10:27 -0500

    M

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from funtions.app import *
 from funtions.servicios import *
-
 from funtions.extras import limpiar_pantalla, cargando
 
 
 # El inventario se define como un diccionario vacío al inicio.
 inventario = {}
-    
+
     # Bucle principal de la aplicación
 while True:
         limpiar_pantalla()
@@ -16,45 +15,46 @@ while True:
             limpiar_pantalla()
             cargando()
             ejecutar_agregar_producto(inventario)
-            
+
         elif opcion == '2':
             limpiar_pantalla()
             cargando()
             srv.mostrar_inventario(inventario)
-            
+        
         elif opcion == '3':
             limpiar_pantalla()
             cargando()
             ejecutar_buscar_producto(inventario)
-            
+        
         elif opcion == '4':
             limpiar_pantalla()
             cargando()
             ejecutar_actualizar_producto(inventario)
-            
+        
         elif opcion == '5':
             limpiar_pantalla()
             cargando()
             ejecutar_eliminar_producto(inventario)
-            
+        
         elif opcion == '6':
             limpiar_pantalla()
             cargando()
             ejecutar_mostrar_estadisticas(inventario)
-            
+        
         elif opcion == '7':
             limpiar_pantalla()
             cargando()
             ejecutar_guardar_inventario(inventario)
-            
+        
         elif opcion == '8':
             limpiar_pantalla()
             cargando()
             ejecutar_cargar_inventario(inventario)
-            
+        
         elif opcion == '9':
             print("\nGracias por usar el sistema. ¡Adiós!")
             break
+
         else:
             limpiar_pantalla()
             cargando()
